92324ef8762146b12bc04e0ede77fc1e45aa5575
Jenny Dinh <jennydinh94@gmail.com>
2024-03-23 18:08:23 +0100

    Add nav menu

--- my-project/src/js/main.js
+++ my-project/src/js/main.js
@@ -1,2 +1,14 @@
 import "../css/main.css";
 import "../../style.css";
+
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const mobileMenu = document.getElementById("mobile-menu");
+const closeMobileMenuIcon = document.getElementById("close-mobile-menu-icon");
+
+hamburgerIcon.addEventListener("click", function () {
+  mobileMenu.classList.remove("hidden");
+});
+
+closeMobileMenuIcon.addEventListener("click", function () {
+  mobileMenu.classList.add("hidden");
+});
