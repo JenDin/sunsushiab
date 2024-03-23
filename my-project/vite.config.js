98931700de706a05857f9c354240891d48567046
Jenny Dinh <jennydinh94@gmail.com>
2024-03-23 21:56:05 +0100

    Removal of dist

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        sushi: resolve(__dirname, "src/sushi.html"),
       },
     },
   },
